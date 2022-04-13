e14c57f25d146258ad59c051359b96647b689ba4
willywin99 <willywinsianturi@gmail.com>
2022-04-14 04:18:11 +0700

    Menuliskan Fungsi dari UsersHandler

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -7,6 +7,7 @@ class UsersHandler {
     this._validator = validator;
 
     this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
   }
 
   async postUserHandler(request, h) {
@@ -48,6 +49,36 @@ class UsersHandler {
   }
 
   async getUserByIdHandler(request, h) {
-    
+    try {
+      const {id} = request.params;
+      const user = await this._service.getUserById(id);
+
+      return {
+        status: 'success',
+        data: {
+          user,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 }
+
+module.exports = UsersHandler;
