cdf92bb957ac7217a60cec9c5a8f43b8de97e5ca
willywin99 <willywinsianturi@gmail.com>
2022-04-14 04:22:20 +0700

    Menuliskan User Plugin

---
+++ src/api/users/index.js
@@ -0,0 +1,10 @@
+const UsersHandler = require('./handler');
+
+module.exports = {
+  name: 'users',
+  version: '1.0.0',
+  register: async (server, {service, validator}) => {
+    const usersHandler = new UsersHandler(service, validator);
+    server.route(routes(usersHandler));
+  },
+};
