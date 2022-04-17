5c2ac59e13bf0b3acbd5d9b47e011f7184fc0b76
willywin99 <willywinsianturi@gmail.com>
2022-04-17 19:12:21 +0700

    Membuat Fungsi deleteRefreshToken

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -28,4 +28,15 @@ class AuthenticationsService {
       throw new InvariantError('Refresh token tidak valid');
     }
   }
+
+  async deleteRefreshToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
 }
+
+module.exports = AuthenticationsService;
