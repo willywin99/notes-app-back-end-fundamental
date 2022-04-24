308d1781570c38952edd12e41b3d1ffc7cb1f3c9
willywin99 <willywinsianturi@gmail.com>
2022-04-24 07:08:06 +0700

    fix typo

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -150,7 +150,8 @@ class NotesHandler {
       if (error instanceof ClientError) {
         const response = h.response({
           status: 'fail',
-          message: 'Catatan gagal dihapus. Id tidak ditemukan',
+          // message: 'Catatan gagal dihapus. Id tidak ditemukan',
+          message: error.message,
         });
         response.code(error.statusCode);
         return response;
