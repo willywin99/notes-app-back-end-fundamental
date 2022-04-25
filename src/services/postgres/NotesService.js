d2f257cf1a70df8f1062567f8a40f787630b4c70
willywin99 <willywinsianturi@gmail.com>
2022-04-25 07:37:27 +0700

    Membuat Fungsi verifyNoteAccess di NotesService

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -7,8 +7,9 @@ const AuthorizationError = require('../../exceptions/AuthorizationError');
 const {mapDBToModel} = require('../utils');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({title, body, tags, owner}) {
@@ -97,6 +98,22 @@ class NotesService {
 
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+    }
+
+    try {
+      await this._collaborationService.verifyCollaborator(noteId, userId);
+    } catch {
+      throw error;
     }
   }
 }
